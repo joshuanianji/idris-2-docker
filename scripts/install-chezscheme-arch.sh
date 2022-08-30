1f6f6d05171987a5286fffa48a49e6fb073b6cc3
Joshua Ji <joshua.ji@jiwhiz.com>
2022-08-30 11:01:07 -0600

    Thread support when building chezscheme

--- scripts/install-chezscheme-arch.sh
+++ scripts/install-chezscheme-arch.sh
@@ -4,5 +4,7 @@
 git clone --depth 1 https://github.com/racket/ChezScheme.git
 cd ChezScheme 
 git submodule init && git submodule update 
+# get boot files
 ./configure --pb && make tarm64le.bootquick
-make && make install
+# build scheme with thread support enabled
+./configure --threads && make && make install
