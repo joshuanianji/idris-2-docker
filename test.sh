445541ee080ec3a77e23c496b63566bd39747fe8
Joshua Ji <joshua.ji@jiwhiz.com>
2023-09-20 03:11:01 +0000

    Delete `test.sh`

--- test.sh
+++
@@ -1,5 +0,0 @@
-# create a image 
-
-docker buildx build --build-arg IDRIS_VERSION=latest --file base.Dockerfile --tag base-v0.6.0:base --load .
-
-docker buildx build --build-arg BASE_IMG=base-v0.6.0:base --file debian.Dockerfile --load .
