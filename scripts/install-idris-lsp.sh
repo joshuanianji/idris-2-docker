80ce5015d1f9bab3ff7ff0cad8e535047978bc72
Joshua Ji <joshua.ji@jiwhiz.com>
2023-12-23 23:48:43 +0000

    load image

--- scripts/install-idris-lsp.sh
+++ scripts/install-idris-lsp.sh
@@ -10,41 +10,32 @@ else
     echo "LSP Version is set to '$IDRIS_LSP_VERSION'"; 
 fi
 
+cd /build/idris2-lsp
+git submodule update --init --recursive
+
+# install idris
+cd /build/idris2-lsp/Idris2
+make bootstrap SCHEME=scheme PREFIX=/usr/local/lib/idris2
+make all PREFIX=/usr/local/lib/idris2
+make install PREFIX=/usr/local/lib/idris2
+
+# make clean PREFIX=/usr/local/lib/idris2
+# make all PREFIX=/usr/local/lib/idris2
+# make install PREFIX=/usr/local/lib/idris2
+# make install-with-src-libs PREFIX=/usr/local/lib/idris2
+# make install-with-src-api PREFIX=/usr/local/lib/idris2
+
 # check if the version is "idris2-0.4.0" or "idris2-0.5.1"
 # these are the "older" supported versions, before the idris2-lsp repo was split a reusable LSP-lib
 if [[ $IDRIS_LSP_VERSION == "idris2-0.4.0" ]] || [[ $TYPST_VERSION == "idris2-0.5.1" ]]; then
     echo "Installing older version of idris2-lsp"
 
+    # Manual install of idris2-lsp 
     cd /build/idris2-lsp
-    git submodule update --init --recursive
-    cd /build/idris2-lsp/Idris2
-    make bootstrap SCHEME=scheme PREFIX=/usr/local/lib/idris2 
     make install PREFIX=/usr/local/lib/idris2
-
-    # Manual install of idris2-lsp 
-    # https://github.com/idris-community/idris2-lsp#manual-installation
-    make install-with-src-libs PREFIX=/usr/local/lib/idris2
-    make install-with-src-api PREFIX=/usr/local/lib/idris2
-
-    # Manual install of idris2-lsp 
-    cd /build/idris2-lsp
-    make install
 else 
+    # if the idris version is not one of the "old" ones, it is either a newer version (0.6.0 and up) or "latest"d
     echo "Installing newer version of idris2-lsp"
-
-    # if the idris version is not one of the "old" ones, it is either a newer version (0.6.0 and up) or "latest"
-    cd /build/idris2-lsp
-    git submodule update --init Idris2
-    cd /build/idris2-lsp/Idris2
-    make bootstrap SCHEME=scheme PREFIX=/usr/local/lib/idris2
-    make all PREFIX=/usr/local/lib/idris2
-    make install PREFIX=/usr/local/lib/idris2
-
-    make clean PREFIX=/usr/local/lib/idris2
-    make all PREFIX=/usr/local/lib/idris2
-    make install PREFIX=/usr/local/lib/idris2
-    make install-with-src-libs PREFIX=/usr/local/lib/idris2
-    make install-with-src-api PREFIX=/usr/local/lib/idris2
 
     # Manually install LSP library and idris2-lsp
     cd /build/idris2-lsp
